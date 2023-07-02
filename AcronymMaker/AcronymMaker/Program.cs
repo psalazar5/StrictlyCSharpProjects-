488568e172de5ffda55d7769b1d3a9fd16e6cba6
Pablo <psalazar5@live.com>
2023-07-02 03:56:28 -0400

    Acronym Generator

---
+++ AcronymMaker/AcronymMaker/Program.cs
@@ -0,0 +1,27 @@
+﻿try
+{
+    //Gather input from user 
+    Console.WriteLine("Please enter a phrase to convert to an acronym : ");
+    string input = Console.ReadLine();
+
+    // Optional exclusions 
+    string exclusions = input.Replace("and", "");
+    string[] wordArr = exclusions.Split(' '); //create an array of words from the sentence then split splits up the whole string up by whatever character we specify 
+    Console.WriteLine(wordArr.Length);
+
+    //Loop through sentence 
+    string acronym = "";
+    for(int i = 0; i < wordArr.Length; i++)
+    {
+        if (wordArr[i] != "") //if wordArr[i] is not equal to a blank string then we can process the word, but if it is then we can ignore it and loop again 
+        {
+            acronym += wordArr[i][0];
+        }
+    }
+    acronym = acronym.ToUpper();
+    Console.WriteLine("Your acronym is: " + acronym);
+}
+catch (Exception e)
+{
+    Console.WriteLine(e.ToString());
+}
