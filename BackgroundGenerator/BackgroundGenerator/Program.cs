8597134a5cb1623c8e811a70f3ea89cb084e5585
Pablo <psalazar5@live.com>
2023-07-24 15:54:35 -0400

    BackgroundGenerator

--- BackgroundGenerator/BackgroundGenerator/Program.cs
+++ BackgroundGenerator/BackgroundGenerator/Program.cs
@@ -3,10 +3,10 @@
 namespace BackgroundGenerator
 {
      class backgroundChanger
-     {//uses of parameters
+     {//Constants used as parameters for the SystemParametersInfo function 
         public const int SPI_SETDESKWALLPAPER = 20; // Used to set wallpaper
-        public const int SPIF_UPDATEINFILE = 1; 
-        public const int SPIF_SENDCHANGE = 2;
+        public const int SPIF_UPDATEINFILE = 1; //Indicates that the change should be written to the user profile 
+        public const int SPIF_SENDCHANGE = 2; // Broadcasts the WM_SETTINGCHANGE message after updating the uder profile.
         //importing dll
         [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)] //string marshalling, getting ready for storage, SetLastError ; indicating whether the colleage sets the error.
         public static extern int SystemParametersInfo(int uAction, int uParam, string lpvParam, int fuWinIni); //using spi to modify certain parts about our user profile.
